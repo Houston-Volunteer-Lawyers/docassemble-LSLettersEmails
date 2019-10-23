2cb3c8d0a3dca2bc0fc71b419792c2877afa04ba
michaelhofrichterHVL <45667758+michaelhofrichterHVL@users.noreply.github.com>
2019-10-22 23:39:33 -0500

    fixing comment

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.LSLettersEmails',
-      version='0.1.23',
+      version='0.1.24',
       description=('LS Letters'),
       long_description='fixed message vs clientmessage',
       long_description_content_type='text/markdown',
